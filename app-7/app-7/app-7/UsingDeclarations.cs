0b0804c9add5f248af25789b7edbb71c3fa7bf25
Ali Delavari <delavaryali@gmail.com>
2022-09-12 21:47:46 +0430

    add 3 files to app 7

---
+++ app-7/app-7/app-7/UsingDeclarations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_7
+{
+    internal class UsingDeclarations
+    {
+        static void UsingOld()
+        {
+            using (var file = new FileStream("input.txt", FileMode.Open))
+            using (var reader = new StreamReader(file))
+            {
+                var s = reader.ReadToEnd();
+
+
+                // Do something with data
+            }
+        }
+
+
+        static void UsingNew(string[] args)
+        {
+            using Stream file = new FileStream("input.txt", FileMode.Open);
+            using StreamReader reader = new StreamReader(file);
+
+            var s = reader.ReadToEnd();
+
+            // Do something with data
+        }
+    }
+}
